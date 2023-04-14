e4f02952f6e2e3eab3c266374767eec2ebacbb7e
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-04-15 05:57:45 +0600

    circle-gallery (#2)

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from "../Hero/hero";
+import Circle from "../Circle/circle";
+import Gallery from "../Gallery/gallery";
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <Circle/>
+            <Gallery/>
         </div>
     );
 };
