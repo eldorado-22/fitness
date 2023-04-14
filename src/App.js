1434b376602d4de7970e20a3726d1dfeb24466e7
eldorado-22 <jumashevv992@gmail.com>
2023-04-14 19:32:13 +0600

    fitness

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/hero";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+          <Header/>
+          <Hero/>
+          <Footer/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header id="header">
+            <div className="container">
+                <div className="header">
+                    <h1>Hello Header</h1>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
