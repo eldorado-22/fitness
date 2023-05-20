fa708c33830eea450e316176d62ff9e83089e524
Eldorado.JM <119120263+eldorado-22@users.noreply.github.com>
2023-05-20 15:31:55 +0600

    contact-about-inforamation (#10)

--- src/pages/About/Contact/Contact.js
+++ src/pages/About/Contact/Contact.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import "./style.scss";
+import {MdEmail} from "react-icons/md";
+import {FaMapMarkedAlt} from "react-icons/fa";
+import {IoPhonePortrait} from "react-icons/io5";
 
 const Contact = () => {
     return (
-        <div>
-
-        </div>
+        <section id="contact" className="py-10">
+            <div className="container">
+                <div className="contact">
+                    <h1 className="text-center text-5xl pb-10  font-extrabold">Our
+                        <span className="text-red-700">Contact</span>
+                    </h1>
+                    <div className="contact--nav  text-white flex justify-around items-center">
+                        <div className="contact--nav flex items-center flex-col">
+                            <IoPhonePortrait className="contact--nav__icons text-center text-red-700  text-7xl"/>
+                            <pre className="text-4xl font-extrabold text-red-700 py-5">Phone</pre>
+                            <a href="tel:+996(708)-229-173" className="text-white">+996(708)-229-173</a>
+                            <a href="tel:+996(708)-229-173" className="text-white">+996(708)-229-173</a>
+                        </div>
+                        <div className="contact--nav__stick"></div>
+                        <div className="contact--nav flex items-center flex-col">
+                            <FaMapMarkedAlt className="contact--nav__icons text-center text-red-700  text-7xl"/>
+                            <pre className="text-4xl font-extrabold text-red-700 py-5">Address</pre>
+                            <p className="text-white">Bishkek, Kyrgyzstan</p>
+                            <a href="https://go.2gis.com/7xbgf" className="text-white">Turusbekov 109/3</a>
+                        </div>
+                        <div className="contact--nav__stick"></div>
+                        <div className="contact--nav flex items-center flex-col">
+                            <MdEmail className="contact--nav__icons text-center text-red-700  text-7xl"/>
+                            <pre className="text-4xl font-extrabold text-red-700 py-5">Email</pre>
+                            <a href="#" className="text-white">eldorado@gmail.com</a>
+                            <a href="#" className="text-white">jumashev@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Text from "./Text/Text";
 import About from "./Hero/about";
 import AboutPhotoGYM from "./About-Photo-GYM";
+import FitnessInformation from "./Fitness-Information";
+import Contact from "./Contact/Contact";
 
 const AboutUS = () => {
     return (
@@ -9,6 +11,8 @@ const AboutUS = () => {
             <About/>
             <Text/>
             <AboutPhotoGYM/>
+            <FitnessInformation/>
+            <Contact/>
         </div>
     );
 };
