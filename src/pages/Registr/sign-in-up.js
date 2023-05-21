c87ee13ed978f5345579f06f7e13f9c3fb386b05
eldorado-22 <jumashevv992@gmail.com>
2023-05-21 13:20:35 +0600

    registration

---
+++ src/pages/Registr/sign-in-up.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./style.scss";
+const SignInUp = () => {
+    return (
+        <div id="registration">
+            <div className="container">
+                <div className="registration">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SignInUp;
