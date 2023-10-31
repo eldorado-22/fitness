a84720adcc1ac551edfd615d63a6c81cea45d72c
eldorado-22 <jumashevv992@gmail.com>
2023-10-31 16:02:20 +0600

    delete_fitness_information

--- src/pages/Registr/sign-in-up.js
+++ src/pages/Registr/sign-in-up.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import uniqid from 'uniqid';
 import "./style.scss";
