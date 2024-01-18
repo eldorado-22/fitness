07796eb2e3f8daa6bd5584bb94da33652bd2b059
eldorado-22 <jumashevv992@gmail.com>
2024-01-18 21:20:06 +0600

    fitness_trainer_second

--- src/pages/Tarainer/trainer.js
+++ src/pages/Tarainer/trainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TrainHero from "./Train_Hero/hero";
+import Train_Categories from "./Train_Categories";
 
 const Trainer = () => {
     return (
         <>
             <TrainHero/>
+            <Train_Categories/>
         </>
     );
 };
